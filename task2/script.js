2c8ca825f01aab9032d343145240d7bc69fb4e69
yasmine sakr <y5257835@gmail.com>
2026-08-01 21:27:20 +0200

    Add task 2

---
+++ task2/script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('changeText');
+const textparagraph = document.getElementById('lead');
+btn.addEventListener('click', function(){
+  textparagraph.innerText ="you can order now and eat the delicious dessert";
+});
