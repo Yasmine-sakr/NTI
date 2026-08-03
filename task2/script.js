3b60b4e93f87d40d7016506e6314f19c20fcceb4
yasmine sakr <y5257835@gmail.com>
2026-08-03 12:14:32 +0200

    update

--- task2/script.js
+++ task2/script.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('changeText');
 const textparagraph = document.getElementById('lead');
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
   textparagraph.innerText ="you can order now and eat the delicious dessert";
 });
+const btn2 = document.querySelector('.btn-warning');
+const featurette2 = document.getElementById('featurette2');
